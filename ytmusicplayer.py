5890389f19dac5f2ece09943b50a2b5b80e1da58
Kim Valkama <kim.valkama@hotmail.com>
2017-06-09 15:43:20 +0300

    Create ytmusicplayer.py

--- ytmusicplayer.py
+++ ytmusicplayer.py
@@ -3,18 +3,16 @@
 import pafy 
 import re
 import urllib
-#import urllib2
 from bs4 import BeautifulSoup
 
 term = input("What u wont m8?: ")
-search = urllib.quote(term)
-url ="https://www.youtube.com/results?search_query=" + search
-response = urllib.urlopen(url)
+url ="https://www.youtube.com/results?search_query=" + term
+response = urllib.request.urlopen(url)
 html = response.read()
-soup = BeautifulSoup(html)
-link = soup.find(attrs={'class':'yt-uix-tile-link'})
+soup = BeautifulSoup(html, "lxml")
+
+link = soup.find('a', href=re.compile("/watch*"))
+
+
 
 print (link)
-
-
-#https://stackoverflow.com/questions/2792650/python3-error-import-error-no-module-name-urllib2
