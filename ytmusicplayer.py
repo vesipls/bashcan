581e7e79f3c8b00fb34b2872896387ead33624a7
Kim Valkama <kim.valkama@hotmail.com>
2017-06-09 22:10:31 +0300

    Update ytmusicplayer.py

--- ytmusicplayer.py
+++ ytmusicplayer.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 
-import pafy 
+import pafy
 import re
 import urllib
 from bs4 import BeautifulSoup
 
-term = input("What u wont m8?: ")
+#Search youtube
+
+term = input("Search term: ")
 url ="https://www.youtube.com/results?search_query=" + term
 response = urllib.request.urlopen(url)
 html = response.read()
 soup = BeautifulSoup(html, "lxml")
 
-link = soup.find('a', href=re.compile("/watch*"))
+#Find video link
 
+link = soup.find('a', attrs={'href': re.compile("/watch*")})
+watchlink = str(link)
+startfind = watchlink.find('href="') + 6
+endfind = watchlink.find('"><div', startfind)
 
+watchfinal =  (watchlink[startfind:endfind])
 
-print (link)
+#print the video link
+
+print (watchfinal)
