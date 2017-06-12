0cf48ef083b893fe145461a93bf59cf16760ff99
Kim Valkama <kim.valkama@hotmail.com>
2017-06-12 13:58:03 +0300

    Create ytfeelinglucky.py

--- ytfeelinglucky.py
+++ ytfeelinglucky.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
-import pafy
 import re
 import urllib
 from bs4 import BeautifulSoup
 
 #Search youtube
-
 term = input("Search term: ")
 url ="https://www.youtube.com/results?search_query=" + term
 response = urllib.request.urlopen(url)
@@ -14,14 +12,11 @@ html = response.read()
 soup = BeautifulSoup(html, "lxml")
 
 #Find video link
-
 link = soup.find('a', attrs={'href': re.compile("/watch*")})
 watchlink = str(link)
 startfind = watchlink.find('href="') + 6
 endfind = watchlink.find('"><div', startfind)
-
 watchfinal =  (watchlink[startfind:endfind])
+pafyurl = "https://www.youtube.com" + watchfinal
 
-#print the video link
-
-print (watchfinal)
+print (pafyurl)
